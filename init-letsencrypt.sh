b415fc4082eca06b15b4c294eb7efa010441bb4f
hsjni0110 <hansukjinid@gmail.com>
2023-07-30 22:59:15 +0900

    fix: init-letsencrypt.sh 오타 수정

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -15,16 +15,16 @@ staging=0 # Set to 1 if you're testing your setup to avoid hitting request limit
 
 if [ -d "$data_path" ]; then
     read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-        if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-            exit
+    if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+        exit
     fi
 fi
 
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
     echo "### Downloading recommended TLS parameters ..."
     mkdir -p "$data_path/conf"
-    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx
-    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pe
+    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
     echo
 fi
 
